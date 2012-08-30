214a03c3d8c4d47022ff2b4ba97119e840d19a62
Jure Žove <jure.pase.kure@gmail.com>
2012-08-30 09:06:38 +0200

    Initial Commit

---
+++ JZUnlock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JZUnlock
+//
+//  Created by Jure Žove on 8/30/12.
+//  Copyright (c) 2012 Jure Žove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JZUnlock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JZUnlock
+//
+//  Created by Jure Žove on 8/30/12.
+//  Copyright (c) 2012 Jure Žove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
